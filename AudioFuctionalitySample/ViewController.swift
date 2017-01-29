15173606eca92248240265e8c68a44623979afa7
Viraj Padte <virajpadte@gmail.com>
2017-01-28 20:25:23 -0700

    added play pause and stop functionality

--- AudioFuctionalitySample/ViewController.swift
+++ AudioFuctionalitySample/ViewController.swift
@@ -7,19 +7,47 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
 
+    @IBOutlet weak var slider: UISlider!
+    
+    var player = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let audioPath = Bundle.main.path(forResource: "kashi", ofType: ".mp3")
+        do {
+            try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+        } catch  {
+            
+        }
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func play(_ sender: Any) {
+        player.play()
+        
+    }
 
-
+    @IBAction func pause(_ sender: Any) {
+        player.pause()
+    }
+    
+    @IBAction func stop(_ sender: Any) {
+        player.stop()
+        player.currentTime = 0
+    }
+    @IBAction func slide(_ sender: Any) {
+         player.volume = slider.value
+    }
+    
+    
 }
 
