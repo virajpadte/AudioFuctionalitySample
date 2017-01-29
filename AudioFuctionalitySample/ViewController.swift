cb6f3b30f432a870b24261744060e2d55d99c00f
Viraj Padte <virajpadte@gmail.com>
2017-01-28 21:25:59 -0700

    added scroll tracing functionality

--- AudioFuctionalitySample/ViewController.swift
+++ AudioFuctionalitySample/ViewController.swift
@@ -10,13 +10,20 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController{
-
-    @IBOutlet weak var slider: UISlider!
+ 
+    
+    @IBOutlet weak var volSlider: UISlider!
+    @IBOutlet weak var scrollSlider: UISlider!
     
     var player = AVAudioPlayer()
     
+    //timer
+    var timer = Timer()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
         let audioPath = Bundle.main.path(forResource: "kashi", ofType: ".mp3")
         do {
@@ -24,6 +31,11 @@ class ViewController: UIViewController{
         } catch  {
             
         }
+        
+        //set scroll max to song duration
+        scrollSlider.maximumValue = Float(player.duration)
+        //reset scroll position
+        scrollSlider.value = 0
     }
 
     
@@ -33,19 +45,30 @@ class ViewController: UIViewController{
     }
     @IBAction func play(_ sender: Any) {
         player.play()
-        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(update), userInfo: nil, repeats: true)
+    
     }
 
     @IBAction func pause(_ sender: Any) {
         player.pause()
+        timer.invalidate()
     }
     
     @IBAction func stop(_ sender: Any) {
         player.stop()
         player.currentTime = 0
+        timer.invalidate()
     }
     @IBAction func slide(_ sender: Any) {
-         player.volume = slider.value
+         player.volume = volSlider.value
+    }
+    
+    func update(){
+        //print(Float(player.currentTime))
+        scrollSlider.value = Float(player.currentTime)
+        if player.currentTime == player.duration{
+            timer.invalidate()
+        }
     }
     
     
